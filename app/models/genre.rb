aab63d9f301c10e8b1057c3682effaa11235c8ba
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-06 17:13:58 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    self.songs.length
+    songs.length
   end
 
   def artist_count
-    self.artists.length
+    artists.length
   end
 
   def all_artist_names
-    self.artists.map(&:name)
+    artists.map(&:name)
   end
 end
