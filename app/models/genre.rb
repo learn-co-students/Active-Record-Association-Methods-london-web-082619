ed891e4927e748d4b741fe28c3e0d528a81fde66
ck2e14 <chriswkennedy@icloud.com>
2019-09-08 18:29:49 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,17 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    # returns the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    # returns the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    # returns an array of strings containing every musician's name by mapping (transforming) over every artist and calling the .name attribute.
+    self.artists.map {|artist| artist.name}
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,15 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name # returns the actual name attribute of the genre associated with the instance of song that the method is called on, rather than returning the genre object (which would include the id and whatever other attributes that each genre might have - in this case just ID)
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
-    # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
-    # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+    # this invokes the ActiveRecord method find_or_create_by - which we manually built in earlier labs - the arguments for this are to give the attribute we want to find/create by, which is name, and then as a string the actual name. Then we are setting the artist association of the instance the method is called on to be Drake, creating it first if 
+    # no Drake artist was found
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
+
 
   end
 end
